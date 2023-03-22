df91b305d2308ba965f082c425131b04b650085e
IdR3n <bidrisarikaya@gmail.com>
2023-03-22 13:37:37 +0100

    Creation de  const Templates

--- main.js
+++ main.js
@@ -2,11 +2,43 @@
 const temp = [
    {
       "id": "1",
-      "name": "Nom du template",
+      "name": "Automaty Gramaty",
       "price": "150",
-      "description": "Description du template",
-      "url": "./..."
+      "description": "Vous ne savez pas depuis combien de temps vous travaillez ? Pas de problème, à partir du moment où vous êtes en ligne sur ce site, le temps est conservé et enregistré dans le calendrier.",
+      "url": "img/1.jpg"
    },
+
+   {
+      "id": "2",
+      "name": "Kick the Boss 2",
+      "price":"125",
+      "description": "Votre patron vous ennuie-t-il ? Attaquez-vous comme si c'était votre patron ! Pas de désintégration ou de déchirure grâce au coton apaisant et à la couture au laser",
+      "url": "img/2.jpg"
+   },
+
+   {
+      "id": "3",
+      "name": "Coil",
+      "price":"100",
+      "description": "La façon la plus délicieuse et merveilleuse de boire du café",
+      "url": "img/3.jpg"
+   },
+
+   {
+      "id": "4",
+      "name": "Robin",
+      "price":"95",
+      "description": "Plus besoin d'être en retard pour les rendez-vous et dates d'affaires importants grâce à l'agenda en ligne.",
+      "url": "img/4.jpg"
+   },
+
+   {
+      "id": "5",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": "temporairement, si vous achetez annuellement, nous payons vos frais mensuels. Site sécurisé avec satellite et connexion sécurisée",
+      "url": "img/5.jpg"
+   }
 ];
 
 const options = [
