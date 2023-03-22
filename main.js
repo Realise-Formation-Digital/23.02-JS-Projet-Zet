db91123949478122a7200afeff4a275c811f16e8
Ariel <126970535+Aryel-Mag@users.noreply.github.com>
2023-03-22 11:13:37 +0100

    base de la partie Js

---
+++ main.js
@@ -0,0 +1,45 @@
+// ----------- Données ----------------------------------------------------------
+const temp = [
+   {
+      "name": "Nom du template",
+      "price": "150",
+      "description": "Description du template"
+   },
+];
+
+const options = [
+   {
+      "name": "Nom de la fonctionnalité",
+      "price": "100",
+      "description": "Description de la fonctionnalité"
+   },
+];
+// ----------- Variables --------------------------------------------------------
+let selectionList = []; // liste du template et des fonctionnalités choisies
+
+// ------------------------------------------------------------------------------
+
+
+
+/**
+ * Fonction qui vide la séléction et ferme la fenêtre
+ */
+function clr(){
+
+}
+
+/**
+ * Prendre lla liste de selection et calcule le prix total et le retourne
+ */
+function calculator(){
+
+   return 0;
+}
+
+
+/**
+ * Affiche le prix total calculé et la liste de la selection
+ */
+function displayPrice(){
+
+}
