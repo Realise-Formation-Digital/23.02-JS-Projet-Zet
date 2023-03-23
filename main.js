d8faedc83ff8b6f21f62bc10ded7fbf6ead4a882
IdR3n <bidrisarikaya@gmail.com>
2023-03-23 09:06:08 +0100

    ajouter nouvelle  templates elements

--- main.js
+++ main.js
@@ -2,43 +2,125 @@
 const temp = [
    {
       "id": "1",
-      "name": "Automaty Gramaty",
+      "name": "Booming Shop",
       "price": "150",
-      "description": "Vous ne savez pas depuis combien de temps vous travaillez ? Pas de problème, à partir du moment où vous êtes en ligne sur ce site, le temps est conservé et enregistré dans le calendrier.",
-      "url": "img/1.jpg"
+      "description": "Vous ne trouvez pas le type d'interface utilisateur que vous souhaitez? Vous voulez des interfaces utilisateur plus délicates ? Alors tu es au bon endroit.",
+      "url": "img/15.jpg"
    },
 
    {
       "id": "2",
       "name": "Kick the Boss 2",
       "price":"125",
-      "description": "Votre patron vous ennuie-t-il ? Attaquez-vous comme si c'était votre patron ! Pas de désintégration ou de déchirure grâce au coton apaisant et à la couture au laser",
-      "url": "img/2.jpg"
+      "description": " ",
+      "url": "img/14.jpg"
    },
 
    {
       "id": "3",
       "name": "Coil",
       "price":"100",
-      "description": "La façon la plus délicieuse et merveilleuse de boire du café",
-      "url": "img/3.jpg"
+      "description": " ",
+      "url": "img/13.jpg"
    },
 
    {
       "id": "4",
       "name": "Robin",
       "price":"95",
-      "description": "Plus besoin d'être en retard pour les rendez-vous et dates d'affaires importants grâce à l'agenda en ligne.",
-      "url": "img/4.jpg"
+      "description": " ",
+      "url": "img/12.jpg"
    },
 
    {
       "id": "5",
       "name": "Template 5",
       "price":"Mensuellement 65-Annuel:715!!",
-      "description": "temporairement, si vous achetez annuellement, nous payons vos frais mensuels. Site sécurisé avec satellite et connexion sécurisée",
+      "description": " ",
+      "url": "img/11.jpg"
+   },
+
+   {
+      "id": "6",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": " ",
+      "url": "img/10.jpg"
+   },
+
+   {
+      "id": "7",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": " ",
+      "url": "img/9.jpg"
+   },
+
+   {
+      "id": "8",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": " ",
+      "url": "img/8.jpg"
+   },
+
+   {
+      "id": "9",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": " ",
+      "url": "img/7.jpg"
+   },
+
+   {
+      "id": "10",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": " ",
+      "url": "img/6.jpg"
+   },
+
+   {
+      "id": "11",
+      "name": "Template 5",
+      "price":"Mensuellement 65-Annuel:715!!",
+      "description": "temporairement, si vous achetez annuellement, nous payons vos frais mensuels. Site sécurisé avec satellite et connexion sécurisée.",
       "url": "img/5.jpg"
+   },
+
+   {
+      "id": "12",
+      "name": "Template 5",
+      "price":"  ",
+      "description": "Plus besoin d'être en retard pour les rendez-vous et dates d'affaires importants grâce à l'agenda en ligne.",
+      "url": "img/4.jpg"
+   },
+
+   {
+      "id": "13",
+      "name": "Coil3",
+      "price":"100",
+      "description": "La façon la plus délicieuse et merveilleuse de boire du café.",
+      "url": "img/3.jpg"
+   },
+
+   {
+      "id": "14",
+      "name": "Kick the Boss 2",
+      "price":"125",
+      "description": "Votre patron vous ennuie-t-il ? Attaquez-vous comme si c'était votre patron ! Pas de désintégration ou de déchirure grâce au coton apaisant et à la couture au laser. ",
+      "url": "img/2.jpg"
+   },
+
+   {
+      "id": "15",
+      "name": "Automaty a gramaty",
+      "price":"150",
+      "description": "Vous ne savez pas depuis combien de temps vous travaillez ? Pas de problème, à partir du moment où vous êtes en ligne sur ce site, le temps est conservé et enregistré dans le calendrier.",
+      "url": "img/1.jpg"
    }
+
+
 ];
 
 const options = [
